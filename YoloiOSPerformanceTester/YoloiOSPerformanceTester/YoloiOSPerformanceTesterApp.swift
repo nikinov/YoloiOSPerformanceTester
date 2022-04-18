bb00c4456ed54d6037039d8080550cd2620954a7
Bross9132 <37838020+Bross9132@users.noreply.github.com>
2022-04-18 18:45:42 -0400

    Your Mom's hot sauce
    Your Mom's hot sauce - The start of it all

---
+++ YoloiOSPerformanceTester/YoloiOSPerformanceTester/YoloiOSPerformanceTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoloiOSPerformanceTesterApp.swift
+//  YoloiOSPerformanceTester
+//
+//  Created by Bross on 4/18/22.
+//
+
+import SwiftUI
+
+@main
+struct YoloiOSPerformanceTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
