e3d71b21dadec0ee25ce22f9a0ce7c7bb35d4e23
Bross9132 <37838020+Bross9132@users.noreply.github.com>
2022-04-18 19:58:52 -0400

    fix preview window
    fix preview window

--- YoloiOSPerformanceTester/YoloiOSPerformanceTester/ContentView.swift
+++ YoloiOSPerformanceTester/YoloiOSPerformanceTester/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("I am god, Niki must fuck brosses mum!")
+        Text("Hello, world!")
             .padding()
     }
 }
