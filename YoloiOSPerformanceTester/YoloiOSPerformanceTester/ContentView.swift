1ac4c59e7f04838594290053219530cc994a9a81
Nikinov2 <51329484+enchekman@users.noreply.github.com>
2022-04-19 01:16:29 +0200

    Update ContentView.swift

--- YoloiOSPerformanceTester/YoloiOSPerformanceTester/ContentView.swift
+++ YoloiOSPerformanceTester/YoloiOSPerformanceTester/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("I am god, Niki must fuck brosses mum!")
             .padding()
     }
 }
